58b3fbc0065e2cf9b458af73a0eff7fc9dbec63f
Ludwig Felder <felder.ludwig@googlemail.com>
2023-08-27 14:16:32 +0200

    feat: added chain collison logic

--- ChessBrawl/Assets/Scripts/MoveAgent.cs
+++ ChessBrawl/Assets/Scripts/MoveAgent.cs
@@ -7,7 +7,16 @@ using Unity.MLAgents.Sensors;
 
 public class MoveAgent : Agent {
 
-    //[SerializeField] private Transform targetTransform;
+    public enum AgentColor
+    {
+        Black,
+        White
+    }
+
+
+    [Header("Agent Configuration")]
+    [SerializeField] private AgentColor agentColor = AgentColor.Black; // Default value
+
     public List<Transform> targetTransforms = new List<Transform>();
 
     [Header("Initial Position Ranges")]
@@ -18,7 +27,10 @@ public class MoveAgent : Agent {
     [SerializeField] private float startZMin = -6.7f;
     [SerializeField] private float startZMax = -6.7f;
 
-    public override void OnEpisodeBegin(){
+    public override void OnEpisodeBegin()
+    {
+        ResetAllPieces();
+
         transform.localPosition = new Vector3(
             Random.Range(startXMin, startXMax),
             Random.Range(startYMin, startYMax),
@@ -32,8 +44,7 @@ public class MoveAgent : Agent {
         foreach (Transform targetTransform in targetTransforms)
         {
             sensor.AddObservation(targetTransform.position);
-        }
-           
+        }      
     }
 
     public override void OnActionReceived(ActionBuffers actions){
@@ -42,7 +53,6 @@ public class MoveAgent : Agent {
 
         float moveSpeed = 1f;
         transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
-
     }
 
     public override void Heuristic(in ActionBuffers actionsOut){
@@ -51,14 +61,51 @@ public class MoveAgent : Agent {
         continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
-    private void OnTriggerEnter(Collider other){
-        if(other.TryGetComponent<Wall>(out Wall wall)){
+    // Reward calculation for the agent.
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check for wall collision.
+        if(other.TryGetComponent<Wall>(out Wall wall))
+        {
             AddReward(-100f);
             EndEpisode();
         }
-        if(other.TryGetComponent<Piece>(out Piece piece)){
-            AddReward(1f);
-            // EndEpisode();
+
+        // Check for piece collision.
+        if(other.TryGetComponent<Piece>(out Piece piece))
+        {
+            // If agent collides with a piece of its own color.
+            if((int)piece.PieceColorValue == (int)this.agentColor)
+            {
+                Debug.Log("Same Color Interaction Detected!");
+                piece.agent = this; // Setting the reference
+                piece.pushTimer = Piece.pushDuration; // Reference to the constant pushDuration from the Piece script
+                AddReward(1f);
+            }
+            // If agent collides directly with a piece of the opposite color.
+            else
+            {
+                // Penalize for touching the opposite piece directly.
+                AddReward(-10f);
+            }
+        }
+    }
+
+    private void ResetAllPieces() 
+    {
+        foreach (Transform targetTransform in targetTransforms) 
+        {
+            Piece piece = targetTransform.GetComponent<Piece>();
+            piece.ResetPiece();
+        }
+    }
+
+    void Start()
+    {
+        GameObject[] pieces = GameObject.FindGameObjectsWithTag("Piece");
+        foreach(GameObject piece in pieces)
+        {
+            targetTransforms.Add(piece.transform);
         }
     }
 
--- ChessBrawl/Assets/Scripts/Piece.cs
+++ ChessBrawl/Assets/Scripts/Piece.cs
@@ -10,18 +10,73 @@ public class Piece : MonoBehaviour
         White
     }
 
+    public MoveAgent agent; // Reference to the agent
+
+    public float pushTimer = 0.0f;
+    public const float pushDuration = 1.0f; // This duration can be adjusted as needed
+
     [Header("Piece Configuration")]
     [SerializeField] private PieceColor pieceColor = PieceColor.Black; // Default value
 
-    // Start is called before the first frame update
-    void Start()
+    public PieceColor PieceColorValue
     {
-        
+        get { return pieceColor; }
+    }
+
+    private Rigidbody rb;
+
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    public void ResetPiece()
+    {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        // Any other properties to reset can be added here.
+
+        if (rb != null) 
+        {
+            rb.velocity = Vector3.zero;  // Reset linear velocity.
+            rb.angularVelocity = Vector3.zero;  // Reset angular (rotational) velocity.
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(pushTimer > 0.0f && other.TryGetComponent<Piece>(out Piece otherPiece) && otherPiece.PieceColorValue != this.PieceColorValue)
+        {
+            // Chain Interaction Detected
+            if(agent != null) 
+            {
+                agent.AddReward(10f);
+                Debug.Log("Chain Interaction Detected!");
+                
+                agent.EndEpisode();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pushTimer > 0.0f)
+        {
+            pushTimer -= Time.deltaTime;
+        }
+        else if(agent != null)
+        {
+            agent = null; 
+        }
     }
 }
