a0e71e313a6e431b2014d8ec2bab44df5760ecaa
Ludwig Felder <felder.ludwig@googlemail.com>
2023-08-27 11:58:21 +0200

    feat: added color selection to piece script

--- ChessBrawl/Assets/Scripts/Piece.cs
+++ ChessBrawl/Assets/Scripts/Piece.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    public enum PieceColor
+    {
+        Black,
+        White
+    }
+
+    [Header("Piece Configuration")]
+    [SerializeField] private PieceColor pieceColor = PieceColor.Black; // Default value
+
     // Start is called before the first frame update
     void Start()
     {
